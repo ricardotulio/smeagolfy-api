34b70f682113507ebb1e5e7b42820eebcca724aa
ricardotulio <ricardo.tulio@fatec.sp.gov.br>
2017-05-12 09:26:15 -0300

    Plural: Implement test that verify if translate single words Verify if translate single words in plural using 'ses'

---
+++ tests/lib/smeagolfy.spec.js
@@ -0,0 +1,6 @@
+import test from 'ava'
+import smeagolfier from '../src/lib/smeagolfier'
+
+test('Should answer a plural with "ses" in place of "s"', t => {
+  t.deepEquals(smeagolfier('eggs'), 'eggses')
+})
