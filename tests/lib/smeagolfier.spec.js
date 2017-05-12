e8305c377b89ab0170e61ec356dd72688a870f69
ricardotulio <ricardo.tulio@fatec.sp.gov.br>
2017-05-12 09:37:23 -0300

    Plural: Implement simple translate function Implement simple function responsible for translate the word 'eggs' to 'egges'

--- tests/lib/smeagolfier.spec.js
+++ tests/lib/smeagolfier.spec.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import smeagolfier from '../src/lib/smeagolfier'
+import smeagolfier from '../../src/lib/smeagolfier'
 
 test('Should answer a plural with "ses" in place of "s"', t => {
   t.deepEquals(smeagolfier('eggs'), 'eggses')
